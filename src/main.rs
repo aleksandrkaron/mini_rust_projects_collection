ff06c9b5033b56a282d615bfece2fcf279442310
Alexandre Caron <alexandrecaron438@pm.me>
2025-02-06 14:31:40 -0500

    Added an option selector to select a unit on the Temperature Converter + added formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,56 @@
 use std::io;
 
 fn main() {
+    println!("\nWelcome to my compilation of mini Rust projects!\n\
+    This program has been written by @aleksandrkaron, from the knowledge I gained from the Rust book.");
+
     loop {
         println!("\n>>> Select a program to run. <<<\n\n\
-        1) Celsius to Fahrenheit Converter\n\
-        2) Fahrenheit to Celsius Converter\n\
-        3) Project 3\n\
-        4) Project 4");
-        // celsius_to_fahrenheit();
+        1) Temperature Converter\n\
+        2) Project 2\n\
+        3) Project 3\n\n\
+        Please enter your input (1-3).");
+
+        let mut option = String::new();
+
+        io::stdin().read_line(&mut option).expect("Failed to read line");
+
+        let option: u32 = match option.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("\nInvalid option");
+                continue;
+            },
+        };
+
+        match option {
+            1 => {
+                temp_converter();
+                break;
+            },
+            2 => {
+                println!("Project 2");
+                break;
+            },
+            3 => {
+                println!("Project 3");
+                break;
+            },
+            _ => println!("Invalid option"),
+        }
+        println!("Selected option: {option}");
+
+    }
+}
+
+fn temp_converter() {
+    print!("{}[2J", 27 as char);
+    println!("\nWelcome to the Temperature Converter!\n");
+    loop {
+        println!(">>> Which temperature unit would you like to convert? <<<\n\n\
+        1) Celsius\n\
+        2) Fahrenheit\n\n\
+        Please enter your input (1-2).");
 
         let mut option = String::new();
 
@@ -30,23 +73,14 @@ fn main() {
                 println!("Fahrenheit to Celsius Converter");
                 break;
             },
-            3 => {
-                println!("Project 3");
-                break;
-            },
-            4 => {
-                println!("Project 4");
-                break;
-            },
             _ => println!("Invalid option"),
         }
         println!("Selected option: {option}");
-
     }
 }
-
 fn celsius_to_fahrenheit() {
-    println!("Enter a temperature in Celsius.");
+    println!("\nYou chose to convert from Celsius to Fahrenheit.");
+    println!("\n >>> Enter a temperature in Celsius. <<<");
 
     let mut temp_in_c = String::new();
 
@@ -56,5 +90,5 @@ fn celsius_to_fahrenheit() {
 
     let temp_in_f = temp_in_c * 1.8 + 32.0;
 
-    println!("{temp_in_c}°C in Fahrenheit is: {temp_in_f}°F.");
+    println!("\n{temp_in_c}°C in Fahrenheit is: {temp_in_f}°F.");
 }
