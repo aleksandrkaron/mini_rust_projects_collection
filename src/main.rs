83ffa9733e99fb431e6ecf0ab331612f9abe88e5
Alexandre Caron <alexandrecaron438@pm.me>
2025-02-06 12:31:02 -0500

    Added conversion from Celsius to Fahrenheit.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use std::io;
 fn main() {
     println!("Enter a temperature in Celsius.");
 
-    let mut temp = String::new();
+    let mut temp_in_c = String::new();
 
-    io::stdin().read_line(&mut temp).expect("Failed to read temperature");
+    io::stdin().read_line(&mut temp_in_c).expect("Failed to read temperature");
 
-    let temp: i32 = temp.trim().parse().expect("Not a valid number");
+    let temp_in_c: f64 = temp_in_c.trim().parse().expect("Not a valid number");
 
-    println!("The temperature is: {temp}.");
+    let temp_in_f = temp_in_c * 1.8 + 32.0;
 
+    println!("{temp_in_c}°C in Fahrenheit is: {temp_in_f}°F.");
 
 }
